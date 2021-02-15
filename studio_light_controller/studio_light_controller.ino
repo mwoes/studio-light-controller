aae6b19b25722fb157dff72032cce2a9db40b828
harborretail <mason_woessner@harborretail.com>
2021-02-15 11:23:02 -0500

    updating firmware sketch

--- studio_light_controller/studio_light_controller.ino
+++ studio_light_controller/studio_light_controller.ino
@@ -4,10 +4,14 @@
 #include <PCA9685.h>
 #include <aREST.h>
 #include <avr/wdt.h>
+#include <EEPROM.h>
 
 #define SS 10    //W5500 CS
 #define RST 7    //W5500 RST
 #define CS 4     //SD CS pin
+
+#define MW 34 //Master Warm OE
+#define MC 35 //Master Cool OE
 
 // SETUP NETWORK / SERVER
 byte mac[] = {0xD2, 0x8A, 0xA7, 0x3C, 0x0C, 0x67};
@@ -63,6 +67,11 @@ void setup() {
   digitalWrite(CS, HIGH);
   digitalWrite(RST,HIGH);
 
+  pinMode(MW, OUTPUT);
+  pinMode(MC, OUTPUT);
+  digitalWrite(MW, LOW);
+  digitalWrite(MC, LOW);
+
   pinMode(encoderPinA_W, INPUT);
   pinMode(encoderPinB_W, INPUT);
   digitalWrite(encoderPinA_W, HIGH);
@@ -111,11 +120,13 @@ void setup() {
   );
   cool.setPWMFrequency(100);
   
-  rest.function("masterWarm", masterWarm);
-  rest.function("masterCool", masterCool);
+  rest.function("warmSet", warmSet);
+  rest.function("coolSet", coolSet);
   rest.function("allOff", allOff);
   rest.function("allOn", allOn);
   rest.function("ch", channel);
+  rest.function("bankSet", bankSet);
+  rest.function("scene", sceneSet);
   rest.variable("warmBank", &warmLightState);
   rest.variable("coolBank", &coolLightState);
   rest.variable("ip", &IP);
@@ -144,12 +155,12 @@ void setup() {
     serializeJson(network, Serial);
   };
 
-  for(int i=0; i<16; i++) {
+  for(int i=0; i<14; i++) {
     warmLaneState[i] = 0;
     warm.setChannelPWM(i, 255);
   };
 
-  for(int i=0; i<16; i++) {
+  for(int i=0; i<14; i++) {
     coolLaneState[i] = 0;
     cool.setChannelPWM(i, 255);
   };
@@ -177,7 +188,7 @@ void loop() {
 
 int allOff() {
   for(int j=255; j>=0; j--) {
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<14; i++) {
       int checkInt = warmLaneState[i];
       if(checkInt >= j) {
         int y = invert(j);
@@ -185,7 +196,7 @@ int allOff() {
         warmLaneState[i] = j;
       };
     };
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<14; i++) {
       int checkInt = coolLaneState[i];
       if(checkInt >= j) {
         int y = invert(j);
@@ -194,6 +205,9 @@ int allOff() {
       };
     };
   };
+
+  digitalWrite(MW, LOW);
+  digitalWrite(MC, LOW);
 
   warmLightState = warmLaneState.as<String>();
   coolLightState = coolLaneState.as<String>();
@@ -205,8 +219,10 @@ int allOff() {
 }
 
 int allOn() {
+  digitalWrite(MW, HIGH);
+  digitalWrite(MC, HIGH);
   for(int j=0; j<190; j++) {
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<14; i++) {
       int checkInt = warmLaneState[i];
       if(checkInt <= j) {
         int y = invert(j);
@@ -214,7 +230,7 @@ int allOn() {
         warmLaneState[i] = j;
       };
     };
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<14; i++) {
       int checkInt = coolLaneState[i];
       if(checkInt <= j) {
         int y = invert(j);
@@ -233,10 +249,60 @@ int allOn() {
   return 1;
 }
 
-int masterWarm(String command) {
+int bankSet(String command) {
   int offCheck = 0;
 
-  for(int i=0; i<16; i++) {
+  String ch = getValue(command,':',0);
+  String val = getValue(command,':',1);
+  int valInt = val.toInt();
+
+  if(ch == "w") {
+    if(valInt > 0) {
+      digitalWrite(MW, HIGH);
+    } else {
+      digitalWrite(MW, LOW);
+    }
+    for(int i=0; i<14; i++) {
+      int coolCheck = coolLaneState[i];
+      offCheck = offCheck + valInt + coolCheck;
+      int y = invert(valInt);
+      warm.setChannelPWM(i, y << 4);
+      warmLaneState[i] = valInt;
+    }
+    warmLightState = warmLaneState.as<String>();
+  } else if(ch == "c") {
+    if(valInt > 0) {
+      digitalWrite(MC, HIGH);
+    } else {
+      digitalWrite(MC, LOW);
+    }
+    for(int j=0; j<14; j++) {
+      int warmCheck = warmLaneState[j];
+      offCheck = offCheck + valInt + warmCheck;
+      int x = invert(valInt);
+      cool.setChannelPWM(j, x << 4);
+      coolLaneState[j] = valInt;
+    }
+    coolLightState = coolLaneState.as<String>();
+  } else {
+    return 0;
+  }
+
+  if(offCheck > 0) {
+    sysStatus["status"] = "lightsOn";
+  } else {
+    sysStatus["status"] = "lightsOff";
+  }
+  
+  systemStatus = sysStatus["status"].as<String>();
+
+  return 1;
+}
+
+int warmSet(String command) {
+  int offCheck = 0;
+
+  for(int i=0; i<14; i++) {
     String val = getValue(command,':',i);
     int valInt = val.toInt();
     int coolCheck = coolLaneState[i];
@@ -247,8 +313,10 @@ int masterWarm(String command) {
   }
 
   if(offCheck > 0) {
+    digitalWrite(MW, HIGH);
     sysStatus["status"] = "lightsOn";
   } else {
+    digitalWrite(MW, LOW);
     sysStatus["status"] = "lightsOff";
   }
   
@@ -258,10 +326,10 @@ int masterWarm(String command) {
   return 1;
 }
 
-int masterCool(String command) {
+int coolSet(String command) {
   int offCheck = 0;
 
-  for(int i=0; i<16; i++) {
+  for(int i=0; i<14; i++) {
     String val = getValue(command,':',i);
     int valInt = val.toInt();
     int warmCheck = warmLaneState[i];
@@ -272,8 +340,10 @@ int masterCool(String command) {
   }
 
   if(offCheck > 0) {
+    digitalWrite(MC, HIGH);
     sysStatus["status"] = "lightsOn";
   } else {
+    digitalWrite(MC, LOW);
     sysStatus["status"] = "lightsOff";
   }
 
@@ -292,11 +362,17 @@ int channel(String command) {
   int channel = chan.toInt();
 
   if(color == "w") {
+    if(valInt > 0) {
+      digitalWrite(MW, HIGH);
+    }
     int y = invert(valInt);
     warm.setChannelPWM(channel, y << 4);
     warmLaneState[channel] = valInt;
     warmLightState = warmLaneState.as<String>();
   } else if(color == "c") {
+    if(valInt > 0) {
+      digitalWrite(MC, HIGH);
+    }
     int y = invert(valInt);
     cool.setChannelPWM(channel, y << 4);
     coolLaneState[channel] = valInt;
@@ -306,6 +382,8 @@ int channel(String command) {
   if(valInt > 0) {
     sysStatus["status"] = "lightsOn";
   } else {
+    digitalWrite(MW, LOW);
+    digitalWrite(MC, LOW);
     sysStatus["status"] = "lightsOff";
   }
 
@@ -372,6 +450,73 @@ void doEncoderB_C() {
   }
 }
 
+int sceneSet(String command) {
+  // OPTIONS ARE w for write OR s for set
+  String action = getValue(command,':',0);
+  String scene = getValue(command,':',1);
+  
+  int sNum = scene.toInt();
+  int addrW = sNum * 14;
+  int addrC = (sNum * 14) + 512;
+
+  int valInt = 0;
+
+  if(sNum <= 10 && (action == "w" || action == "s")) {
+    if(action == "w") {
+      for(int i=0; i<14; i++) {
+        addrW = addrW + i;
+        EEPROM.update(addrW, warmLaneState[i]);
+      };
+      for(int j=0; j<14; j++) {
+        addrC = addrC + j;
+        EEPROM.update(addrC, coolLaneState[j]);
+      }
+    } else if(action == "s") {
+      int test = 0;
+      for(int i=0; i<14; i++) {
+        addrW = addrW + i;
+        int val = EEPROM.read(addrW);
+        test += val;
+        int y = invert(val);
+        warm.setChannelPWM(i, y << 4);
+        warmLaneState[i] = val;
+        valInt += val;
+      };
+      if(test > 0) {
+        digitalWrite(MW, HIGH);
+      } else {
+        digitalWrite(MW, LOW);
+      }
+      warmLightState = warmLaneState.as<String>();
+      test = 0;
+      for(int j=0; j<14; j++) {
+        addrC = addrC + j;
+        int val = EEPROM.read(addrC);
+        test += val;
+        int y = invert(val);
+        cool.setChannelPWM(j, y << 4);
+        coolLaneState[j] = val;
+        valInt += val;
+      };
+      if(test > 0) {
+        digitalWrite(MC, HIGH);
+      } else {
+        digitalWrite(MC, LOW);
+      }
+      coolLightState = coolLaneState.as<String>();
+    }
+  
+    if(valInt > 0) {
+      sysStatus["status"] = "lightsOn";
+      systemStatus = sysStatus["status"].as<String>();
+    };
+  
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 // ch = cool or warm -- 0=warm 1=cool
 void updatePCA(uint8_t value, bool adjust, int ch) {
   int offCheck = 0;
@@ -381,7 +526,7 @@ void updatePCA(uint8_t value, bool adjust, int ch) {
     if(adjust) { 
       offCheck = offCheck + value;
     };
-    for(int i=0; i<16; i++) {
+    for(int i=0; i<14; i++) {
       if(ch == 0) {
         laneInt = warmLaneState[i];
       } else if(ch == 1) {
@@ -406,6 +551,9 @@ void updatePCA(uint8_t value, bool adjust, int ch) {
         int checkInt = warmLaneState[i];
         int coolCheck = coolLaneState[i];
         offCheck = offCheck + laneInt + coolCheck;
+        if(checkInt > 0 && laneInt > 0) {
+          digitalWrite(MW, HIGH);
+        }
         if(laneInt != checkInt) {
           int y = invert(laneInt);
           warm.setChannelPWM(i, y << 4);
@@ -415,6 +563,9 @@ void updatePCA(uint8_t value, bool adjust, int ch) {
         int checkInt = coolLaneState[i];
         int warmCheck = warmLaneState[i];
         offCheck = offCheck + laneInt + warmCheck;
+        if(checkInt > 0 && laneInt > 0) {
+          digitalWrite(MC, HIGH);
+        }
         if(laneInt != checkInt) {
           int y = invert(laneInt);
           cool.setChannelPWM(i, y << 4);
@@ -426,6 +577,8 @@ void updatePCA(uint8_t value, bool adjust, int ch) {
     if(offCheck > 0) {
       sysStatus["status"] = "lightsOn";
     } else {
+      digitalWrite(MW, LOW);
+      digitalWrite(MC, LOW);
       sysStatus["status"] = "lightsOff";
     }
 
@@ -505,6 +658,8 @@ void readBtns() {
     analogWrite(systemOffLED, 255);
     analogWrite(systemOnLED, 0);
   } else if(systemStatus == "lightsOff") {
+    digitalWrite(MW, LOW);
+    digitalWrite(MC, LOW);
     analogWrite(systemOffLED, 0);
     analogWrite(systemOnLED, 255);
   }
